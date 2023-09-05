6f364e40845bbac417b5dbe611f759203016a5bd
Gergo06-py <borbelygergo06@gmail.com>
2023-09-05 12:08:16 +0200

    Kert letrehozasa

--- src/App.java
+++ src/App.java
@@ -1,16 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Maci maci = new Maci();
-        Macska mici = new Macska();
-        maci.hangotad("Brumm...");
-        maci.mezgyujtes();
-        // maci.nev = "Brumi";
-
-        mici.hangotad("Miauu...");
-        mici.dorombolas();
-        // mici.nev = "aa";
-        mici.beallitNev("Mici");
-        // System.out.println(mici.nev);
-        System.out.println(mici.lekerNev());
+        Kert kert = new Kert();
+        kert.inditElet();
     }
 }
---
+++ src/Kert.java
@@ -0,0 +1,5 @@
+public class Kert {
+    public void inditElet() {
+        
+    }
+}
