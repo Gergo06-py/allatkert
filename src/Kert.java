e311a445266006cda5b644cf4e1d63274fb08c69
Gergo06-py <borbelygergo06@gmail.com>
2023-09-05 12:21:02 +0200

    Ora vege

--- src/Kert.java
+++ src/Kert.java
@@ -1,5 +1,30 @@
 public class Kert {
+    Maci maci;
+    Macska mici;
+
     public void inditElet() {
-        
+        initObject();
+        startMaci();
+        startMici();
+    }
+
+    public void initObject() {
+        maci = new Maci();
+        mici = new Macska();
+    }
+
+    public void startMaci() {
+        maci.hangotad("Brumm...");
+        maci.mezgyujtes();
+        // maci.nev = "Brumi";
+    }
+
+    public void startMici() {
+        mici.hangotad("Miauu...");
+        mici.dorombolas();
+        // mici.nev = "aa";
+        mici.beallitNev("Mici");
+        // System.out.println(mici.nev);
+        System.out.println(mici.lekerNev());
     }
 }
